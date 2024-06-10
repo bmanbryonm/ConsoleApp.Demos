0db60a0222f8f774c08f2a923800506cbd731601
Bryon Montgomery <bryon.montgomery@gmail.com>
2024-06-09 22:00:02 -0500

    Added Loop demos and modified calculator (added Fibonacci calc)

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//10
+//Console.WriteLine("*****************Simple For Loop*****************");
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"Hello World! - {i + 1}");
+//}
+
+//Console.WriteLine("How many times do you want to print Hello World?");
+//int userInput = Convert.ToInt32(Console.ReadLine());
+
+//for (int i= 0; i < userInput; i++)
+//{
+//    Console.WriteLine($"Hello World! {i + 1}");
+//}
+
+//While Loop
+Console.WriteLine("*****************Simple While Loop*****************");
+
+//int counter = 0;
+//while (counter < 10)
+//{
+//    Console.WriteLine($"Hello World! - {counter}");
+//    counter++;
+//}
+
+//int sum = 0;
+//int num = 0;
+//Console.WriteLine("Please enter the numbers you want summed. (enter -1 to exit the program):");
+
+//while (num != -1)
+//{
+//    num = Convert.ToInt32(Console.ReadLine());
+//    if (num != -1)
+//    {
+//        sum += num;
+//    }
+//}
+
+//Console.WriteLine($"The sum of your numbers is {sum}");
+
+//Console.WriteLine("*****************While Loop completed*****************");
+
+//Do While Loop
+Console.WriteLine("*****************Do While Loop*****************");
+
+int sum = 0;
+int num = 0;
+
+do
+{
+    Console.WriteLine("Enter the numbers you want summed.  Enter -1 to exit:");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num != -1)
+    {
+        sum += num;
+    }
+
+} while (num != -1);
+
+Console.WriteLine($"The sum of your numbers is: {sum}");
+
+Console.WriteLine("*****************Do While Loop completed*****************");
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
+
+
+
+Console.ReadKey();
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,49 +1,104 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Welcome message
-Console.WriteLine("********** - Welcome to the sample calculator! - **********");
+//// Welcome message
+//Console.WriteLine("********** - Welcome to the sample calculator! - **********");
 
-// Prompt for user input
-Console.WriteLine("Please enter the first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+//// Prompt for user input
+//Console.WriteLine("Please enter the first number: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Please enter the second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Please enter the second number: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
 
-// Show calculator options
+//// Show calculator options
 
-bool shallExit = false;
-while(!shallExit)
+//bool shallExit = false;
+//while (!shallExit)
+//{
+//    Console.WriteLine("********** - Welcome to the sample calculator! - **********");
+//    Console.WriteLine("What do you want to do?\n[A]dd\n[S]ubtract\n[M]ultiply\n[D]ivide\n[E]xit");
+//    var userInput = Console.ReadLine().ToUpper();
+
+//    switch (userInput)
+//    {
+//        case "A":
+//            Console.WriteLine($"Addition: {num1} + {num2} = " + (num1 + num2));
+//            break;
+//        case "S":
+//            Console.WriteLine($"Subtraction: {num1} - {num2} = " + (num1 - num2));
+//            break;
+//        case "M":
+//            Console.WriteLine($"Multiplication: {num1} x {num2} = " + (num1 * num2));
+//            break;
+//        case "D":
+//            Console.WriteLine($"Division: {num1} / {num2} = " + (num1 / num2));
+//            break;
+//        case "E":
+//            shallExit = true;
+//            Console.WriteLine("Exit program");
+//            break;
+//        default:
+//            Console.WriteLine("Invalid entry");
+//            break;
+
+//    }
+//}
+
+//Console.WriteLine($"The result is: {answer}/n (Press any key to close)");
+
+Console.WriteLine("\n********** - Welcome to the sample calculator! - **********\n\n");
+
+//Show calculator options / show menu
+Console.WriteLine("Please select an operation: (-1 to exit the program\n1. Addition\n2. Subraction\n3. Multiplication\n4. Division\n5. Fibonacci sequence\n");
+int userChoice = Convert.ToInt32(Console.ReadLine());
+
+while (userChoice != -1)
 {
-    Console.WriteLine("********** - Welcome to the sample calculator! - **********");
-    Console.WriteLine("What do you want to do?\n[A]dd\n[S]ubtract\n[M]ultiply\n[D]ivide\n[E]xit");
-    var userInput = Console.ReadLine().ToUpper();
+    Console.WriteLine("Please enter the first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
 
-    switch (userInput)
+    Console.WriteLine("Please enter the second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    int answer = 0;
+
+    switch (userChoice)
     {
-        case "A":
-            Console.WriteLine($"Addition: {num1} + {num2} = " + (num1 + num2));
+        case 1:
+            answer = num1 + num2;
             break;
-        case "S":
-            Console.WriteLine($"Subtraction: {num1} - {num2} = " + (num1 - num2));
+        case 2:
+            answer = num1 - num2;
             break;
-        case "M":
-            Console.WriteLine($"Multiplication: {num1} x {num2} = " + (num1 * num2));
+        case 3:
+            answer = num1 * num2;
             break;
-        case "D":
-            Console.WriteLine($"Division: {num1} / {num2} = " + (num1 / num2));
+        case 4:
+            answer = num1 / num2;
             break;
-        case "E":
-            shallExit = true;
-            Console.WriteLine("Exit program");
+        case 5:
+            for (int i = num1; i <= num2; i++)
+            {
+                answer += i;
+            }
             break;
         default:
-            Console.WriteLine("Invalid entry");
+            Console.WriteLine("Invalid choice");
             break;
 
     }
+
+    Console.WriteLine($"The result is: {answer} \n (Press any key to close)");
+    Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("\n\nPlease select an operation: (-1 to exit the program)\n1. Addition\n2. Subraction\n3. Multiplication\n4. Division\n5. Fibonacci sequence\n");
+    userChoice = Convert.ToInt32(Console.ReadLine());
+
 }
 
+Console.WriteLine("\n\n********** - Thank you for using the sample calculator! - **********");
 
+
+Console.ReadKey();
 
 
